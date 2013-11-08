62bba600ef4a9b88bd581043d2103364db4569ab
anatolikurtsevich <anatoli.kurtsevich@gmail.com>
2013-11-08 18:24:56 +0300

    reformatted code

--- app/js/controllers/headercontroller.js
+++ app/js/controllers/headercontroller.js
@@ -1,17 +1,17 @@
 'use strict';
 
 angular.module('fantasyApp.controllers.header', ['fantasyApp.services.login'])
-  .controller('HeaderController', ['$scope', '$location', 'loginService', 'angularFire', 'FBURL', 
-    function($scope, $location, loginService, angularFire, FBURL) {
+    .controller('HeaderController', ['$scope', '$location', 'loginService', 'angularFire', 'FBURL',
+        function ($scope, $location, loginService, angularFire, FBURL) {
 
-      $scope.$on("angularFireAuth:login", function() {
+            $scope.$on("angularFireAuth:login", function () {
 
-      });
+            });
 
-      $scope.logout = function() {
-        loginService.logout('/signin');
-      };
+            $scope.logout = function () {
+                loginService.logout('/signin');
+            };
 
-      $scope.navbarEntries = [
-      ];
-    }])
+            $scope.navbarEntries = [
+            ];
+        }]);
--- app/js/controllers/signincontroller.js
+++ app/js/controllers/signincontroller.js
@@ -1,23 +1,27 @@
 'use strict';
 
 angular.module('fantasyApp.controllers.signin', ['fantasyApp.services.login'])
-  .controller('SigninCtrl', ['$scope', 'loginService', '$location',
-    function($scope, loginService, $location) {
+    .controller('SigninCtrl', ['$scope', 'loginService', '$location',
+        function ($scope, loginService, $location) {
 
-      $scope.$on('angularFireAuth:login', function () {
-        $location.path('/');
-      })
+            if (!!$scope.auth){
+                $location.path('/');
+            }
 
-      $scope.email = null;
-      $scope.pass = null;
-      $scope.name = null;
+            $scope.$on('angularFireAuth:login', function () {
+                $location.path('/');
+            });
 
-      $scope.login = function(callback) {
-        $scope.err = null;
-        loginService.login($scope.email, $scope.pass, '/', function(err, user) {
-          $scope.err = err||null;
-          typeof(callback) === 'function' && callback(err, user);
-        });
-      };
-    }])
+            $scope.email = null;
+            $scope.pass = null;
+            $scope.name = null;
+
+            $scope.login = function (callback) {
+                $scope.err = null;
+                loginService.login($scope.email, $scope.pass, '/', function (err, user) {
+                    $scope.err = err || null;
+                    typeof(callback) === 'function' && callback(err, user);
+                });
+            };
+        }]);
   
